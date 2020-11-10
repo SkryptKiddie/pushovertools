25322243a2508a74bd29a3097e2dcb206809cef1
Joshua <16878148+SkryptKiddie@users.noreply.github.com>
2020-11-10 10:59:07 +0800

    converted pushoverModule into a python3 module
    can be imported with `import pushover`

--- pushover.py
+++ pushover.py
@@ -1,32 +1,47 @@
-import argparse, requests, os, sys
-from sys import exit
+import requests, os, sys
+class pushoverConfig(): # pushover module in python3
+    def __init__(self):
+        self.api_key = "" # api key, should be defined in the main program
+        self.user_key = "" # user key, should also be defined in the main program
+        self.api_endpoint = "https://api.pushover.net/1/messages.json" # pushover API endpoint
+        self.verify_ssl = False # define whether to check SSL
 
-parser = argparse.ArgumentParser(description="Pushover message relayer")
-parser.add_argument("title", type=str, help="message title")
-parser.add_argument("msg", type=str, help="message body")
-parser.add_argument("priority", type=int, help="priority, valid options are -2, -1, 1 or 2")
-args = parser.parse_args()
+config = pushoverConfig() # define config class
 
-class pushoverauth:
-    api_key = "" # pushover application key
-    user_key = "" # recipient user key
-
-def sendMessage():
-    print("Sending message...")
+def sendMessage(title, message, priority):
+    """Send a normal text message with a title, body and priority."""
     body = {
-        "token": pushoverauth.api_key,
-        "user": pushoverauth.user_key,
-        "title": str(args.title),
-        "message": str(args.msg),
-        "priority": str(args.priority)
-    }
-    response = requests.post("https://api.pushover.net/1/messages.json", body, verify=False)
+        "token": config.api_key,
+        "user": config.user_key,
+        "title": str(title), # message title
+        "message": str(message), # message body
+        "priority": str(priority)} # message priority, valid options are -2 -1 1 2
+    response = requests.post(config.api_endpoint, body, verify=config.verify_ssl)
     if (str(response)[10:-1]) == "[200]":
-        print("Message sent!")
-        sys.exit()
+        return True # success
     else:
-        print("An error occured and your message was not sent.")
-        sys.exit()
+        return False # error
 
-print("Pushover message pusher")
-sendMessage()
+def sendAttachment(title, message, attachment, priority):
+    """Send a message with a title, body, attachment and priority. Attachment must contain Content-Disposition and Content-Type headers."""
+    body = {
+        "token": config.api_key,
+        "user": config.user_key,
+        "title": str(title), # message title
+        "message": str(message), # message body
+        "attachment": str(attachment), # message attachment
+        "priority": str(priority)} # message priority
+    response = requests.post(config.api_endpoint, body, verify=config.verify_ssl)
+    if (str(response)[10:-1]) == "[200]":
+        return True # success
+    else:
+        return False # error
+
+def checkRatelimit():
+    """Check how many requests you have made to the API with your API key."""
+    response = requests.get("https://api.pushover.net/1/apps/limits.json?token={}".format(config.api_key), verify=config.verify_ssl)
+    if response.status_code == 200:
+        return str(response.json())
+    else:
+        return "Bad token was passed"
+    
